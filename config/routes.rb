4b0cd2057b238f359b8002d90f79ea68927bdd6b
Pol-Erwan <polerwankite@gmail.com>
2019-11-05 15:31:43 +0100

    update 15h32

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # get 'static_pages/index'
-  # root 'events_controller#index'
-  root 'static_pages#index'
+  root 'events#index'
+
+  resources :events
+
   get 'static_pages/secret'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
