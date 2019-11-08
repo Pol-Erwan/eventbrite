65f614bb2190adafd13b76802779be84b71ca2bf
Pol-Erwan <polerwankite@gmail.com>
2019-11-08 16:04:25 +0100

    création d'évenements

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -18,6 +18,11 @@ class EventsController < ApplicationController
   end
 
   def create
-
+		@event=Event.new(title: params[:title],price:params[:price],description:params[:description],administrator:current_user.first_name,location: params[:location],start_date:params[:start_date],duration:params[:duration])
+		if @event.save
+			redirect_to event_path(@event.id)
+		else
+			redirect_to new_event_path
+		end
   end
 end
